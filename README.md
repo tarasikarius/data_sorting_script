dc0bad826b38e3c7f052acd9e2b4d4472830535f
tarasikarius <tkonkevich@gmail.com>
2017-02-10 18:14:27 +0200

    Add Readme file

---
+++ README.md
@@ -0,0 +1,2 @@
+To run command inside root directory:
+php public/run.php  --name='value' --stars='value'
---
+++ app/Services/DataGenerator.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Services;
+
+class DataGenerator
+{
+    public function generateFile($filters, $format)
+    {
+        return 'Wo=ho';
+    }
+}
